cc09554e3489d43bd44aab7eb19469454a56308a
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-09-04 16:52:01 +0200

    Booking component changes

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Logo from "../assets/Logo.png";
-import { MdOutlineModeNight } from "react-icons/md";
 
 const Booking = () => {
   return (
     <div className="h-screen w-full bg-no-repeat bg-bookingBackground bg-cover grid place-items-center bg-brightness-[60%] text-light">
-      <div className="bg-dark p-6 rounded-lg">
+      <div className="bg-dark p-4 screen3:p-6 rounded-lg">
         <div className="flex items-center justify-center px-[.3rem]">
           <img src={Logo} alt="Mountain Hideaway" className="w-[14rem]" />
         </div>
@@ -18,15 +17,19 @@ const Booking = () => {
             />
           </div>
           <div>
-            <p>Duration</p>
-            <button className="flex items-center gap-1 px-2 py-[.4rem] bg-light rounded-lg text-dark mt-1">
-              <span>2 nights</span>
-              <MdOutlineModeNight />
-            </button>
+            <p>Check-Out</p>
+            <input
+              type="date"
+              className="mt-1 p-[.2rem] border-[1px] border-dark rounded-lg px-1 text-dark"
+            />
           </div>
         </div>
-        <div className="flex justify-end">
-          <button className="mt-8 px-2 py-1 bg-accLight text-light rounded-lg">
+
+        <div className="flex items-center justify-between mt-8">
+          <a href="/" className="px-2 py-1 bg-gray-500 rounded-lg">
+            Cancel
+          </a>
+          <button className=" px-2 py-1 bg-accLight text-light rounded-lg">
             Check Availability
           </button>
         </div>
