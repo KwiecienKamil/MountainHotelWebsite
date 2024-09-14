ec806ffaa2251e3169d3e3d1854710ee1dd87e45
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-09-14 16:29:24 +0200

    Room booking design rearrange

--- src/components/ui/RoomCard.jsx
+++ src/components/ui/RoomCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const RoomCard = ({ src, price, title }) => {
   return (
-    <div className="bg-dark text-light w-1/4">
-      <img src={src} alt="alt" className="w-[30rem]" />
+    <div className="flex justify-between text-dark w-1/4">
+      <img src={src} alt="alt" className="w-[12rem]" />
       <div className="flex items-center justify-between px-[1.5rem]">
         <div className="flex items-center gap-2 text-xl font-semibold py-4">
           <p>{`${price}$`}</p>
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import Logo from "../assets/Logo.png";
-import Reveal from "../components/ui/Reveal";
 import { AiOutlineLoading } from "react-icons/ai";
 import { toast } from "react-toastify";
 import {
@@ -10,6 +8,7 @@ import {
 } from "../utils/Helpers";
 import dayjs from "dayjs";
 import RoomCard from "../components/ui/RoomCard";
+import Logo from "../assets/Logo.png";
 
 const Booking = () => {
   const [checkInDate, setCheckInDate] = useState("");
@@ -39,75 +38,105 @@ const Booking = () => {
   };
 
   return (
-    <div className="h-screen w-full bg-no-repeat bg-bookingBackground bg-cover flex items-center justify-center flex-col gap-4 bg-brightness-[60%] text-light">
-      <Reveal>
-        <div className="bg-dark p-4 screen3:p-6 rounded-lg">
-          <div className="flex items-center justify-center px-[.3rem]">
-            <img src={Logo} alt="Mountain Hideaway" className="w-[14rem]" />
-          </div>
-          <h1 className="text-center font-semibold pr-2">
-            {formattedTodaysDate}
-          </h1>
-          <div className="flex items-center justify-between gap-[6rem] mt-4 ">
-            <div>
-              <p>Check-In</p>
-              <input
-                type="date"
-                className="mt-1 p-[.2rem] border-[1px] border-dark rounded-lg px-1 text-dark cursor-pointer"
-                onChange={(event) => setCheckInDate(event.target.value)}
+    <div className="min-h-screen flex items-center flex-col gap-4 text-light bg-no-repeat bg-bookingBackground bg-cover bg-brightness-[60%] font-mont">
+      <div className="fixed w-[60%] h-screen bg-white z-0 flex items-center justify-center">
+        {showRooms ? (
+          <div className="">
+            {availableRooms.map((room) => (
+              <RoomCard
+                src={room.image}
+                title={room.title}
+                price={room.price}
               />
-            </div>
-            <div>
-              <p>Check-Out</p>
-              <input
-                type="date"
-                className="mt-1 p-[.2rem] border-[1px] border-dark rounded-lg px-1 text-dark cursor-pointer"
-                onChange={(event) => setCheckOutDate(event.target.value)}
-              />
-            </div>
+            ))}
           </div>
-          {showRooms ? (
-            <div className="pt-4">
-              {availableRooms.map((room) => (
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center gap-4 text-xl">
-                    <span>{`${room.price}$`}</span>
-                    <h2>{room.title}</h2>
-                  </div>
-                  <div className="flex items-center gap-4 my-2 text-sm">
-                    <button className="px-2 py-1 bg-white text-dark">
-                      Preview
-                    </button>
-                    <button className="px-2 py-1 bg-white text-dark">
-                      Select
-                    </button>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ) : null}
-          <div className="flex items-center justify-between mt-8">
-            <a
-              href="/"
-              className="px-2 py-1 bg-gray-500 rounded-lg hover:opacity-[70%] duration-300"
-            >
-              Cancel
-            </a>
-            <button
-              className=" px-2 py-1 bg-accLight text-light rounded-lg hover:opacity-[70%] duration-300"
-              onClick={CheckRoomsAvailability}
-            >
-              {loading ? (
-                <AiOutlineLoading className="animate-spin text-xl" />
-              ) : (
-                <p>Check Availability</p>
-              )}
-            </button>
+        ) : (
+          <h2 className="text-gray-500 text-lg">
+            Pick the perfect dates for your stay and click "Search Rooms"
+          </h2>
+        )}
+      </div>
+      <div className="flex items-center justify-center gap-8 w-full bg-dark px-[4rem] z-10">
+        <div className="flex items-center gap-8 py-8">
+          <div className="flex items-center gap-4">
+            <p>Check-In</p>
+            <input
+              type="date"
+              className="p-[.1rem] border-[1px] border-dark rounded-lg px-1 text-dark cursor-pointer"
+              onChange={(event) => setCheckInDate(event.target.value)}
+            />
+          </div>
+          <div className="flex items-center gap-4">
+            <p>Check-Out</p>
+            <input
+              type="date"
+              className="p-[.1rem] border-[1px] border-dark rounded-lg px-1 text-dark cursor-pointer"
+              onChange={(event) => setCheckOutDate(event.target.value)}
+            />
           </div>
         </div>
-      </Reveal>
+        <button
+          className=" px-2 py-1 bg-accLight text-light rounded-lg hover:opacity-[70%] duration-300"
+          onClick={CheckRoomsAvailability}
+        >
+          {loading ? (
+            <AiOutlineLoading className="animate-spin text-xl" />
+          ) : (
+            <p>Search Rooms</p>
+          )}
+        </button>
+      </div>
     </div>
   );
 };
 
 export default Booking;
+
+{
+  /* <Reveal>
+<div className="bg-dark p-4 screen3:p-6 rounded-lg">
+  <div className="flex items-center justify-center px-[.3rem]">
+    <img src={Logo} alt="Mountain Hideaway" className="w-[14rem]" />
+  </div>
+  <h1 className="text-center font-semibold pr-2">
+    {formattedTodaysDate}
+  </h1>
+  <div className="flex items-center justify-between gap-[6rem] mt-4 ">
+    <div>
+      <p>Check-In</p>
+      <input
+        type="date"
+        className="mt-1 p-[.2rem] border-[1px] border-dark rounded-lg px-1 text-dark cursor-pointer"
+        onChange={(event) => setCheckInDate(event.target.value)}
+      />
+    </div>
+    <div>
+      <p>Check-Out</p>
+      <input
+        type="date"
+        className="mt-1 p-[.2rem] border-[1px] border-dark rounded-lg px-1 text-dark cursor-pointer"
+        onChange={(event) => setCheckOutDate(event.target.value)}
+      />
+    </div>
+  </div>
+  <div className="flex items-center justify-between mt-8">
+    <a
+      href="/"
+      className="px-2 py-1 bg-gray-500 rounded-lg hover:opacity-[70%] duration-300"
+    >
+      Cancel
+    </a>
+    <button
+      className=" px-2 py-1 bg-accLight text-light rounded-lg hover:opacity-[70%] duration-300"
+      onClick={CheckRoomsAvailability}
+    >
+      {loading ? (
+        <AiOutlineLoading className="animate-spin text-xl" />
+      ) : (
+        <p>Check Availability</p>
+      )}
+    </button>
+  </div>
+</div>
+</Reveal> */
+}
